d86ce7625abfdf14a665fcabb17fb7ea4b879552
ardy1110 <ardia0908@gmail.com>
2022-12-15 22:50:49 +0700

    last commit

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://candid-bublanina-8eef4e.netlify.app/" // TODO: replace this
 const studentName = "ARDIANSYAH" // TODO: replace this
 const studentId = "FE2796383" // TODO: replace this
 
